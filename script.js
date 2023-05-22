4af2432c6ab39a2468e51d4cfb67c68e393aa884
Umair-Jamaat-Ali <umairansari758@gmail.com>
2023-05-22 17:03:30 +0500

    Operators

---
+++ script.js
@@ -0,0 +1,44 @@
+//// Operaters
+//// Basic operators (+, -, *, /)
+var a = 5;
+var b = 9;
+
+console.log("Result of sum ", a + b);
+
+console.log("Result of substact ", a - b);
+
+console.log("Result of multiply ", a * b);
+
+console.log("Result of dividion ", a + b);
+
+/// Comparison operation ( ===, !=, <, >, <=, >=)
+
+
+console.log("a is equal to b ", a === b);
+
+console.log("a is not equal to b ", a != b);
+
+console.log("a is greater than to b ", a > b);
+
+console.log("a is lessthan to b ", a < b);
+
+console.log("a is equal to b ", a <= b);
+
+console.log("a is equal to b ", a >= b);
+
+
+//// Logical Operators
+
+
+var c = true;
+var d = false;
+
+/// AND opereation
+
+console.log("AND result is", c && d);
+
+//// OR operator
+
+console.log("OR result is", c || d);
+
+
