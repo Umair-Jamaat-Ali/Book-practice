2105bb31bb137724e8f797400041035fceafaad6
Umair-Jamaat-Ali <umairansari758@gmail.com>
2023-05-23 19:51:06 +0500

    practice

--- script.js
+++ script.js
@@ -42,3 +42,94 @@ console.log("AND result is", c && d);
 console.log("OR result is", c || d);
 
 
+
+///// If - else
+
+//// if statement
+
+var age = 20;
+
+if(age > 18){
+    console.log("You are Qualified for drived");
+};
+
+//// if-else statement
+
+var age1 = 15;
+
+if (age1 >= 18) {
+    console.log("You are Qualified for drived");
+} else {
+    console.log("You are not qualified for driving");
+};
+
+
+///// if and else-if statement
+
+var book = "math";
+
+if(book === history){
+    console.log("you select history book");
+}
+
+else if (book === "economics") {
+    console.log("you select economics book");
+}
+
+else{
+    console.log("you select math book");
+}
+
+
+///// Switch 
+
+var grade = "A";
+
+switch (grade) {
+    case "A":
+        console.log("Your grade is excelent");
+        break;
+        case "B":
+            console.log("Your grade is pretty good");
+            break;
+            case "C":
+        console.log("Your grade is good");
+        break;
+        case "D":
+        console.log("Your grade is not good");
+        break;
+
+        case "E":
+        console.log("Your grade is bad");
+        break;
+    default:
+        console.log("you are failed");
+        break;
+}
+
+
+/////// While Loop
+
+var x = 1;
+ while (x <= 10) {
+    console.log("while loop " , x);
+    x++;
+ };
+
+ ///// Do Loop
+
+ var y = 0;
+ do {
+    console.log("do-while ", y);
+    y++;
+ } while (y = 0);
+
+
+ ///// for loop
+
+ var z = 5;
+
+ for (let index = 1; index <= 10; index++) {
+    console.log(index + " x " + z + " = " + index*z);
+    
+ }
